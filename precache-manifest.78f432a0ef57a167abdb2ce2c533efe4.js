46f3bc879a1f2de0fa2a428113e25c6ee2ba313f
Danis96 <danis.preldzic@gmail.com>
2019-11-20 08:25:48 +0100

    Updates

---
+++ precache-manifest.78f432a0ef57a167abdb2ce2c533efe4.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c8173482c06ea17df986c509ee75d3c2",
+    "url": "/shirtdesign/index.html"
+  },
+  {
+    "revision": "0678d4f0e0bd20351036",
+    "url": "/shirtdesign/static/css/main.2d352d26.chunk.css"
+  },
+  {
+    "revision": "6349aba68763fbcd3842",
+    "url": "/shirtdesign/static/js/2.c6d70e05.chunk.js"
+  },
+  {
+    "revision": "0678d4f0e0bd20351036",
+    "url": "/shirtdesign/static/js/main.81ee74a5.chunk.js"
+  },
+  {
+    "revision": "37e12ce36db79bcd1042",
+    "url": "/shirtdesign/static/js/runtime-main.b5e84de3.js"
+  },
+  {
+    "revision": "f3d6280bc10fcf52f63c8af22a3888bf",
+    "url": "/shirtdesign/static/media/cat.f3d6280b.png"
+  }
+]);
